d52811f9e3996e1038da1a0ddf3d8a16ad29c276
Malcolm <lorbem@rpi.edu>
2018-04-08 16:43:47 -0400

    first working version, fixed up logical errors

--- yx5300.cpp
+++ yx5300.cpp
@@ -1,7 +1,7 @@
 #include "yx5300.h"
 #include "yx5300_defines.h"
 yx5300::yx5300(int rx, int tx): mp3_stream(rx,tx){
-    //mp3_stream = SoftwareSerial(rx, tx);
+    
 }
 //yx5300::yx5300(Stream s){
 //    mp3_stream = s;
@@ -31,19 +31,26 @@ int yx5300::set_volume(int8_t volume){
 void yx5300::startup()
 {
     mp3_stream.begin(9600);
-    executeCommand(CMD_SEL_DEV, 0, 0x02);
+    delay(500);
+    sendCommand(CMD_SEL_DEV, 0, 0x02);
+    delay(500);
 }
 
 int yx5300::executeCommand(int8_t command, int8_t arg1, int8_t arg2)
 {
     //send the command, receive an answer
     sendCommand(command, arg1, arg2);
+    while(!mp3_stream.available()){}
     receiveAnswer();
+    //for(int i=0;i<10;i++){
+    //Serial.print(reply_buffer[i]);
+    //}
+    //Serial.println();
     //parse the answer to check for errors and status
 }
 
 void yx5300::sendCommand(int8_t command, int8_t arg1, int8_t arg2){
-    delay(5);
+    delay(20);
     send_buffer[0] = 0x7e;   // Starting byte, always 7E
     send_buffer[1] = 0xff;   // second byte, always FF
     send_buffer[2] = 0x06;   // Length of command
@@ -59,7 +66,10 @@ void yx5300::sendCommand(int8_t command, int8_t arg1, int8_t arg2){
 void yx5300::receiveAnswer(){
     int bytes_received = 0;
     while (mp3_stream.available() && bytes_received < 10){
-	reply_buffer[bytes_received] = mp3_stream.read();
+	uint8_t i = mp3_stream.read();
+	//Serial.print(i);
+	reply_buffer[bytes_received] = i;
 	bytes_received++;
+	
     }
 }
