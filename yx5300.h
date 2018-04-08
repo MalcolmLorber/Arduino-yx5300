c82f3c35489a59a13fb07dc7de3c999789e010e2
MalcolmLorber <malcolmlorber@gmail.com>
2018-04-08 13:38:41 -0400

    fleshing out api a bit

--- yx5300.h
+++ yx5300.h
@@ -7,8 +7,17 @@
 class yx5300{
 	public:
 		yx5300(int rx, int tx);
+		int play();
+		int pause();
+		int next();
+		int prev();
+		int volume_up();
+		int volume_down();
+		int set_volume();
 	private:
 		int rx_pin, tx_pin;
+		int8_t send_buffer;
+		uint8_t reply_buffer;
 };
 
 #endif
