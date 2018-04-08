7039ccfff741fd0a9ca4c5b6d2b1e379369b6d49
MalcolmLorber <malcolmlorber@gmail.com>
2018-04-08 14:35:54 -0400

    updating structure to better match arduino guidelines

--- yx5300.h
+++ yx5300.h
@@ -7,15 +7,16 @@
 class yx5300{
 	public:
 		yx5300(int rx, int tx);
+		yx5300(Stream s);
 		int play();
 		int pause();
 		int next();
 		int prev();
 		int volume_up();
 		int volume_down();
-		int set_volume();
+		int set_volume(int volume);
 	private:
-		int rx_pin, tx_pin;
+		Serial mp3_stream;
 		int8_t send_buffer;
 		uint8_t reply_buffer;
 };
