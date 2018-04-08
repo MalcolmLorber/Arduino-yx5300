0f6dc8495cb7da1ae12412846f02c099f34f47ca
Malcolm <lorbem@rpi.edu>
2018-04-08 15:03:50 -0400

    added send and receive bodies, fixed up header, formatting

--- yx5300.cpp
+++ yx5300.cpp
@@ -1,10 +1,10 @@
 #include "yx5300.h"
 
 yx5300::yx5300(int rx, int tx){
-	mp3_stream = SoftwareSerial(rx, tx);
+    mp3_stream = SoftwareSerial(rx, tx);
 }
 yx5300::yx5300(Stream s){
-	mp3_stream = s;
+    mp3_stream = s;
 }
 int yx5300::play();
 int yx5300::pause();
@@ -13,5 +13,24 @@ int yx5300::prev();
 int yx5300::volume_up();
 int yx5300::volume_down();
 int yx5300::set_volume(int volume);
-void yx5300::sendCommand(int8_t command, int16_t arguments);
-String yx5300::receiveAnswer();
+void yx5300::sendCommand(int8_t command, int16_t arguments){
+    delay(5);
+    send_buffer[0] = 0x7e;   //
+    send_buffer[1] = 0xff;   //
+    send_buffer[2] = 0x06;   // Len
+    send_buffer[3] = command;//
+    send_buffer[4] = 0x01;   // 0x00 NO, 0x01 feedback
+    send_buffer[5] = (int8_t)(arguments >> 8);  //datah
+    send_buffer[6] = (int8_t)(arguments);       //datal
+    send_buffer[7] = 0xef; //
+    for(uint8_t i = 0; i<8; i++){
+	mp3_stream.write(send_buffer[i]);
+    }
+}
+void yx5300::receiveAnswer(){
+    int bytes_received = 0;
+    while (mp3_stream.available() && bytes_received < 10){
+	reply_buffer[bytes_received] = mp3_stream.read();
+	bytes_received++;
+    }
+}
--- yx5300.h
+++ yx5300.h
@@ -5,22 +5,22 @@
 #include <SoftwareSerial.h>
 
 class yx5300{
-	public:
-		yx5300(int rx, int tx);
-		yx5300(Stream s);
-		int play();
-		int pause();
-		int next();
-		int prev();
-		int volume_up();
-		int volume_down();
-		int set_volume(int volume);
-	private:
-		void sendCommand(int8_t command, int16_t arguments);
-		String receiveAnswer();
-		Serial mp3_stream;
-		int8_t send_buffer[8];
-		uint8_t reply_buffer[10];
+ public:
+    yx5300(int rx, int tx);
+    yx5300(Stream s);
+    int play();
+    int pause();
+    int next();
+    int prev();
+    int volume_up();
+    int volume_down();
+    int set_volume(int volume);
+ private:
+    void sendCommand(int8_t command, int16_t arguments);
+    void receiveAnswer();
+    Serial mp3_stream;
+    int8_t send_buffer[8];
+    uint8_t reply_buffer[10];
 };
 
 #endif
